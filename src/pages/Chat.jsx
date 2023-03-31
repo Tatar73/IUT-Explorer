0d4e6e421fc6d3cc288eb3aa9582b94edca9cc14
Tatar73 <119617920+Tatar73@users.noreply.github.com>
2023-03-31 17:03:04 +0200

    page start.jsx front + début chat.jsx

---
+++ src/pages/Chat.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import robot from '../assets/img/chatImg.svg'
+const Chat = () => {
+  return (
+    <div className="chatPage">
+        <div className="header">
+            <div className="left">
+                <h1>Chat Bot</h1>
+                <div className="statut">
+                    <div className="circle"></div>
+                    <p>en ligne</p>
+                </div>
+            </div>
+            <div className="right">
+                <img src={robot} alt="illustration robot" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Chat
