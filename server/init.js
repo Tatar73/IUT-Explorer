3db2698824db5be65fbe5a2012a41315d9912782
Tatar73 <119617920+Tatar73@users.noreply.github.com>
2023-04-24 09:37:04 +0200

    update init.js all questions

--- server/init.js
+++ server/init.js
@@ -7,12 +7,32 @@ mongoose.connect('mongodb://localhost:27017/chatbotDB', { useNewUrlParser: true,
   .then(async() => {
     console.log('Connected to MongoDB');
     const question1 = new Schema({
-        question: 'Vers quelle formation souhaitez-vous vous diriger ?',
-        reponse: ['GACO','GCCD','MMI', 'PEC', 'SGM', 'Ne sait pas']
+      question: 'Vers quelle formation souhaitez-vous vous diriger ?',
+      reponse: ['GACO','GCCD','MMI', 'PEC', 'SGM', 'Ne sait pas']
     })
     const question1a = new Schema({
       question: 'Parmi ces 5 domaines, lequel préférez-vous : ',
       reponse: ['Ingénierie matériaux','Packaging innovation','Multimédia internet', 'Génie civil', 'Gestion commerce ']
+    })
+    const question1b = new Schema({
+      question: 'Quelle branche préférez-vous : ',
+      reponse: ['Développement web','Graphisme','Communication', 'Audiovisuel']
+    })
+    const question1c = new Schema({
+      question: 'Quelle branche préférez-vous : ',
+      reponse: ['métiers du recyclage et de la valorisation des matériaux',"métiers de l'ingénierie des matériaux et des produits","métiers de la caractérisation et de l'expertise des matériaux et des produits"]
+    })
+    const question1d = new Schema({
+      question: 'Quelle branche préférez-vous : ',
+      reponse: ['éco-conception et industrialisation','éco-conception homologation supply chain']
+    })
+    const question1e = new Schema({
+      question: 'Quelle branche préférez-vous : ',
+      reponse: ['travaux bâtiment','travaux publics', 'réhabilitation et amélioration des performances environnementales des bâtiments', "bureaux d'études conception"]
+    })
+    const question1f = new Schema({
+      question: 'Quelle branche préférez-vous : ',
+      reponse: ['management responsable de projet et des organisations','management des fonctions supports', 'management commercial et marketing omnicanal', "management des activités culturelles, artistiques, sportives et de tourisme"]
     })
     const question2 = new Schema({
       question: 'Souhaitez vous avoir des informations sur les études aménagées ?',
@@ -28,6 +48,12 @@ mongoose.connect('mongodb://localhost:27017/chatbotDB', { useNewUrlParser: true,
     })
 
     await question1.save()
+    await question1a.save()
+    await question1b.save()
+    await question1c.save()
+    await question1d.save()
+    await question1e.save()
+    await question1f.save()
     await question2.save()
     await question3.save()
     await question4.save()
