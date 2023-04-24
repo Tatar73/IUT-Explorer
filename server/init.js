320c6e70fba04a930c420897476a01f1a2338cc5
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2023-04-24 09:34:02 +0200

    Questions init.js

--- server/init.js
+++ server/init.js
@@ -14,8 +14,23 @@ mongoose.connect('mongodb://localhost:27017/chatbotDB', { useNewUrlParser: true,
       question: 'Parmi ces 5 domaines, lequel préférez-vous : ',
       reponse: ['Ingénierie matériaux','Packaging innovation','Multimédia internet', 'Génie civil', 'Gestion commerce ']
     })
+    const question2 = new Schema({
+      question: 'Souhaitez vous avoir des informations sur les études aménagées ?',
+      reponse: ['Musique','Art','Sport', 'A distance', 'SGM', 'Non je ne suis pas intéressé(e)']
+    })
+    const question3 = new Schema({
+      question: 'Voulez vous vous renseignez sur  :',
+      reponse: ["Années à l’étranger (Erasmus)",'Activités sportives sur le campus','Associations dans le campus ', 'Aucune de ces 2 réponses']
+    })
+    const question4 = new Schema({
+      question: 'Je voudrais:',
+      reponse: ["partir en Erasmus à l’étranger ",'faire du sport ',"m’engager auprès d’une association", 'aucune de ces 3 réponses']
+    })
 
     await question1.save()
+    await question2.save()
+    await question3.save()
+    await question4.save()
     console.log('save');
   })
   .catch((error) => console.log(error.message));
